432c68f0b1ee33b430a642c968d460153f2c21ce
Saba-Var <vartasashvili94@gmail.com>
2024-03-16 23:05:30 +0400

    feat: add sign-up functionality with email verification

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Controllers\Api;
 
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Auth\Events\Registered;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\SignUpRequest;
+use Illuminate\Http\JsonResponse;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+	public function signUp(SignUpRequest $request): JsonResponse
+	{
+		$data = $request->validated();
+
+		$user = User::create($data);
+
+		event(new Registered($user));
+
+		Auth::loginUsingId($user->id);
+
+		return response()->json([
+			'message' => 'Successfully created user!',
+		], 201);
+	}
+
+	public function emailVerify(EmailVerificationRequest $request): JsonResponse
+	{
+		$request->fulfill();
+
+		return response()->json([
+			'message' => 'Email verified!',
+		], 200);
+	}
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['controller' => AuthController::class, 'prefix' => 'auth'], function () {
+	Route::post('sign-up', 'signUp')->middleware('throttle:3,1')->name('auth.sign-up');
+	Route::get('email/verify/{id}/{hash}', 'emailVerify')->middleware(['auth', 'signed'])->name('verification.verify');
+});
