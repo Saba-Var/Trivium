bca0212230bd2075fd9015af3016d51e95322b33
Saba-Var <vartasashvili94@gmail.com>
2024-03-17 13:34:17 +0400

    feat: login and logout routes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Auth\Events\Registered;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\SignUpRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use App\Models\User;
 
 class AuthController extends Controller
@@ -22,8 +24,10 @@ class AuthController extends Controller
 
 		Auth::loginUsingId($user->id);
 
+		$this->regenerateSession($request);
+
 		return response()->json([
-			'message' => 'Successfully created user!',
+			'message' => 'Successfully created user! Please verify your email.',
 		], 201);
 	}
 
@@ -34,5 +38,51 @@ class AuthController extends Controller
 		return response()->json([
 			'message' => 'Email verified!',
 		], 200);
+	}
+
+	public function logIn(LoginRequest $request): JsonResponse
+	{
+		$credentials = $request->validated();
+
+		$user = $this->getUserWithVerifiedEmail($credentials['email']);
+
+		$isMatch = $user && password_verify($credentials['password'], $user->password);
+
+		if ($isMatch) {
+			Auth::loginUsingId($user->id, array_key_exists('remember', $credentials) ? true : false);
+			$this->regenerateSession($request);
+
+			return response()->json([
+				'message' => 'Logged in!',
+			], 200);
+		}
+
+		return response()->json([
+			'email'    => 'The provided credentials are incorrect.',
+			'password' => 'The provided credentials are incorrect.',
+		], 401);
+	}
+
+	public function logOut(Request $request, User $user): JsonResponse
+	{
+		auth()->guard('web')->logout();
+
+		$request->session()->invalidate();
+		$this->regenerateSession($request);
+
+		return response()->json([
+			'message' => 'Logged out!',
+		], 200);
+	}
+
+	private function regenerateSession(Request $request): void
+	{
+		$request->session()->regenerate();
+		$request->session()->regenerateToken();
+	}
+
+	private function getUserWithVerifiedEmail($email): User|null
+	{
+		return User::where('email', $email)->whereNotNull('email_verified_at')->first();
 	}
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,14 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+	Route::group(['prefix' => 'auth', 'controller' => AuthController::class], function () {
+		Route::post('log-out', 'logout')->name('auth.log-out');
+	});
+});
+
 Route::group(['controller' => AuthController::class, 'prefix' => 'auth'], function () {
-	Route::post('sign-up', 'signUp')->middleware('throttle:3,1')->name('auth.sign-up');
+	Route::post('sign-up', 'signUp')->name('auth.sign-up');
+	Route::post('log-in', 'logIn')->name('auth.log-in');
 	Route::get('email/verify/{id}/{hash}', 'emailVerify')->middleware(['auth', 'signed'])->name('verification.verify');
 });
