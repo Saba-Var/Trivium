5673b0e2dc532be900530bb0ed45707b693ac453
Saba-Var <vartasashvili94@gmail.com>
2024-03-17 21:19:38 +0400

    feat: create answer model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Question extends Model
 {
@@ -19,5 +20,10 @@ class Question extends Model
 	public function quiz(): BelongsTo
 	{
 		return $this->belongsTo(Quiz::class);
+	}
+
+	public function answers(): HasMany
+	{
+		return $this->hasMany(Answer::class);
 	}
 }
