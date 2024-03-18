0b4e6a26f8ac71349ba5dec5da653c2167dba61c
Saba-Var <vartasashvili94@gmail.com>
2024-03-18 10:26:19 +0400

    feat: create quiz nova resource

--- app/Enums/QuizDifficulty.php
+++ app/Enums/QuizDifficulty.php
@@ -9,4 +9,10 @@ class QuizDifficulty
 	const MEDIUM = 'medium';
 
 	const HARD = 'hard';
+
+	const Lables = [
+		self::EASY   => 'Easy',
+		self::MEDIUM => 'Medium',
+		self::HARD   => 'Hard',
+	];
 }
---
+++ app/Nova/Quiz.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use App\Enums\QuizDifficulty as QuizDifficultyEnum;
+
+class Quiz extends Resource
+{
+	/**
+	 * The model the resource corresponds to.
+	 *
+	 * @var class-string<\App\Models\Quiz>
+	 */
+	public static $model = \App\Models\Quiz::class;
+
+	/**
+	 * The single value that should be used to represent the resource when being displayed.
+	 *
+	 * @var string
+	 */
+	public static $title = 'title';
+
+	/**
+	 * The columns that should be searched.
+	 *
+	 * @var array
+	 */
+	public static $search = [
+		'id',
+		'title',
+	];
+
+	/**
+	 * Get the fields displayed by the resource.
+	 *
+	 * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+	 *
+	 * @return array
+	 */
+	public function fields(NovaRequest $request)
+	{
+		return [
+			ID::make()->sortable(),
+
+			Text::make('Title')
+				->sortable()
+				->rules('required', 'max:100', 'min:3'),
+
+			Number::make('Time')
+				->sortable()
+				->rules('required', 'min:1', 'max:60'),
+
+			Select::make('difficulty')
+				->options(QuizDifficultyEnum::Lables)
+				->displayUsingLabels()
+				->sortable()
+				->rules('required'),
+
+			Textarea::make('Description')
+				->sortable()
+				->rules('required', 'max:255', 'min:4'),
+		];
+	}
+
+	/**
+	 * Get the cards available for the request.
+	 *
+	 * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+	 *
+	 * @return array
+	 */
+	public function cards(NovaRequest $request)
+	{
+		return [];
+	}
+
+	/**
+	 * Get the filters available for the resource.
+	 *
+	 * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+	 *
+	 * @return array
+	 */
+	public function filters(NovaRequest $request)
+	{
+		return [];
+	}
+
+	/**
+	 * Get the lenses available for the resource.
+	 *
+	 * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+	 *
+	 * @return array
+	 */
+	public function lenses(NovaRequest $request)
+	{
+		return [];
+	}
+
+	/**
+	 * Get the actions available for the resource.
+	 *
+	 * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+	 *
+	 * @return array
+	 */
+	public function actions(NovaRequest $request)
+	{
+		return [];
+	}
+}
