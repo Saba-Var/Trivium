82406397e8b2eb192be5874068b045c32a742771
Saba-Var <vartasashvili94@gmail.com>
2024-03-23 20:39:45 +0400

    feat: animate statistics numbers

--- client/src/components/landing/StatisticBlock/StatisticBlock.vue
+++ client/src/components/landing/StatisticBlock/StatisticBlock.vue
@@ -1,14 +1,31 @@
 <script setup lang="ts">
 import TopRightArrowIcon from '@/components/icons/TopRightArrowIcon.vue'
 import type { Props } from './types'
+import { ref, watch } from 'vue'
 
 const props = defineProps<Props>()
+
+const number = ref(0)
+
+watch(
+  () => props.count,
+  (newVal) => {
+    number.value = 0
+    const interval = setInterval(() => {
+      if (number.value < newVal) {
+        number.value += 1
+      } else {
+        clearInterval(interval)
+      }
+    }, 100)
+  }
+)
 </script>
 
 <template>
   <div :class="`text-white py-20 px-12 lg:pt-7 lg:pb-16 lg:pr-40 ${props.className}`">
     <p class="text-7xl font-black flex flex-col">
-      {{ !props.count ? 0 : Math.floor(props.count / 5) * 5 + '+' }}
+      {{ number + '+' }}
     </p>
 
     <RouterLink to="/quizzes" class="flex gap-6 items-center">
