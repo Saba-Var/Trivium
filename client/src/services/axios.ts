e556901ff1bf1c7a5399cfc996b44a22d6cd6a3d
Saba-Var <vartasashvili94@gmail.com>
2024-03-24 12:37:11 +0400

    feat: navigate to internal server error page with axios if 500 status code returned

--- client/src/services/axios.ts
+++ client/src/services/axios.ts
@@ -1,6 +1,21 @@
 import { axiosConfig } from '@/config/axiosConfig'
+import router from '@/router'
 import Axios from 'axios'
 
 const axios = Axios.create(axiosConfig)
+
+axios.interceptors.response.use(
+  async (response) => {
+    return response
+  },
+
+  async (error) => {
+    if (error?.response?.status === 500) {
+      router.push({ name: 'InternalServerError' })
+    }
+
+    return Promise.reject(error)
+  }
+)
 
 export { axios }
